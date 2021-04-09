38e9e9fd6d0e10e4b86e90e94bd9b42aa9d133cc
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-04-09 17:00:38 -0300

    Resolve bug in create method.

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
@@ -25,4 +27,8 @@ public class PersonController {
         return personService.createPerson(personDTO);
     }
 
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
 }
