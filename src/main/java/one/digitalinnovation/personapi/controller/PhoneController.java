c21a55a966f895e82de296f9d7f409cd77c759f9
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-15 16:21:44 -0300

    Allowed all cross origin

--- src/main/java/one/digitalinnovation/personapi/controller/PhoneController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PhoneController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/phone")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
